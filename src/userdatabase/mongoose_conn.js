36fb21dee05d23ed0d6d10a78f748615c3cee534
amanbhai1 <49727102+amanbhai1@users.noreply.github.com>
2024-05-30 01:02:31 +0530

    update

--- src/userdatabase/mongoose_conn.js
+++ src/userdatabase/mongoose_conn.js
@@ -1,9 +1,6 @@
 const mongoose = require("mongoose");
-const dotenv = require('dotenv').config();
 const registeration = require('./userdata.js')
 
-const username = process.env.MONGOOSE_USERNAME;
-const password = process.env.MONGOOSE_PASSWORD;
 
 mongoose.connect(`mongodb+srv://amangupta121:Aman2003@cluster0.o4jamu0.mongodb.net/Users`)
 .then(()=>{
